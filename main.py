dfdffa6d4d4074a1bf16d63b38c5606a837d77a5
ngvinhy <ngvinhy@users.noreply.github.com>
2022-12-01 17:02:02 +0700

    ngvinhy

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from atm import atm
 title = 'CHƯƠNG TRÌNH GIAO DỊCH TẠI CÂY ATM'
 print(title.center(60))
+# giả sử list người dùng chỉ có 5 thành viên trong nhóm
 tenkh = ['Nguyễn Vĩnh Ý', 'Nguyễn Thị Thu Hiền', 'Đinh Hồng Cơ', 'Bùi Đăng Nguyên', 'Đỗ Thành Danh']
 tentk = ['y', 'hien', 'co', 'nguyen', 'danh']
 mk = [1, 2, 3, 4, 5]
 sodu = [30000000, 40000000, 50000000, 60000000, 70000000]
 hanmuc = [5000000, 10000000, 15000000, 20000000, 30000000]
-nganhang = [100000000]
+# giả sử ngân hàng có 10 triệu
+nganhang = [10000000]
 while True:
     name = str(input('Hãy nhập tên tài khoản: '))
     if name in tentk:
